f11a92c553c3a2979923c38dcbcfa7e99a597d64
V-Karch <luna.karch05@gmail.com>
2024-11-12 20:11:53 -0500

    added very basic bot that just sends a message in any channel where a user sends a message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Discord.WebSocket;
+
+internal class Program 
+{
+    private readonly DiscordSocketClient client; 
+    private string token = File.ReadAllText("../../../token.txt");
+
+    public Program()
+    {
+        this.client = new DiscordSocketClient();
+        this.client.MessageReceived += MessageHandler;
+    }
+
+    public async Task StartBotAsync()
+    {
+        await this.client.LoginAsync(Discord.TokenType.Bot, token);
+        await this.client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+    private async Task MessageHandler(SocketMessage message)
+    {
+        if (message.Author.IsBot) return;
+
+        await ReplyAsync(message, "C# Response works!");
+    }
+
+    private async Task ReplyAsync(SocketMessage message, string response)
+    {
+        await message.Channel.SendMessageAsync(response);
+    }
+
+    static async Task Main(string[] args)
+    {
+        var myBot = new Program();
+        await myBot.StartBotAsync();
+    }
+}
